5c9594d537b7f376bce3b2f0e8c3143b0cce1296
madhusudanupadhyay <upadhyay.ms01@gmail.com>
2017-02-25 03:58:57 +0530

    Added secretSession for user and updated file

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,5 +1,11 @@
 exports.render = function(req, res) {
-    res.render('index.ejs', {
+    if (req.session.lastVisit) {
+        console.log(req.session.lastVisit);
+    }
+
+    req.session.lastVisit = new Date();
+
+    res.render('index', {
         title: 'Hello World'
     });
 };
--- config/env/development.js
+++ config/env/development.js
@@ -1,3 +1,4 @@
 module.exports = {
     //Development Configuration Options
+    sessionSecret: 'developmentSessionSecret'
 };
